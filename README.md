59f7b6d630c598e13a065143cd760a7815ef0034
Jackson Trudel <jtrudel97@gmail.com>
2021-10-20 22:25:53 -0400

    Fixed EOF issue and cleaned files

--- README.md
+++ README.md
@@ -1,11 +1,28 @@
 # steganography
-Simple C program which offers utilities for substitution steganography. It can hide a message within a .bmp file and discover messages it has hidden. 
 
+Simple C program which offers utilities for substitution steganography. It can hide a message within a .bmp file and discover messages it has hidden.
 
 ## How to use this program
+
 Simply compile and run the program. Follow the prompts to hide messages in bitmap ('.bmp') files, as well as recover hidden messages. The program also enables duplicating files.
 
-## This program is not perfect - still a WIP
-Still working out some unexpected behaviors. This program *DOES* work well for ".bmp" input files consisting of one color (very limiting, I know..). An working example can be found in "test_case_A/". The input file is "test_case_A/no_message.bmp".
-An failing example can be found in "test_case_B/". The input file is "test_case_B/no_message.bmp". The message "test_case_B/secret_message.txt" was hidden in "no_message.bmp" to make "test_case_B/message_hidden.bmp".  
+## Making (.bmp) image files:
 
+I created these test cases using Paint 3D (exported in .bmp format). This will store the (.bmp) file in a way (without compression or a color table) which is compatible with this program. See note in next section about compatibility issues.
+
+## If something fails:
+
+Make sure you specified the correct file extensions for both input and output files.
+
+This program is only designed to work with (.bmp) files, but with minor changes it could work with any image file which stores the uncompressed value of each pixel.
+
+If it is not working correctly, this could be due to the type of (.bmp) file passed as the original input:
+
+- The (.bmp) must not be compressed
+- The (.bmp) must not store a color table, just the pixel data. This program has not been tested with (.bmp) files which store a color table as metadata.
+  The test cases were created using Paint 3D, which will export in a format compatible with this program.
+
+## Todo:
+
+- Detect if the (.bmp) file is compressed, and halt the operation
+- Encrypting message using a password
--- steganography.c
+++ steganography.c
@@ -1,7 +1,9 @@
 // Jackson Trudel
 // Basic steganography program (via substitution) which works with bitmap files (.bmp)
 // Can also duplicate files
-// Note: This does not work well for files/bitmaps containing FF (pure B/R/G or all) => (major issue)
+// Note: This assumes the most simple (.bmp) file format, where the
+//         color table is omitted.
+//       This program may not work with all (.bmp) files
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -14,14 +16,27 @@ enum UserACtion
     QUIT = 9
 };
 
+enum PrintFormat
+{
+    BYTES,
+    SUFFIX
+};
+
 int printMenu();
 void hideMessage();
 void printMessage();
 void duplicateFile();
-void printPrettyFileSize(unsigned long long size);
-void printCharBinary(char in);
+void copyBytes(FILE *ifp, FILE *ofp, unsigned long long numBytes);
+void printPrettyNumber(unsigned long long size, int format);
 
-#define COPY_BUFFER_SIZE 10000
+// constants
+#define STD_BUFFER_SIZE 1024
+#define HEADER_SIZE 14
+#define INFO_HEADER_SIZE 40
+#define MESSAGE_SIZE_SECTION_SIZE 64
+#define MAX_INPUT_LINE_LENGTH 10000
+#define MIN_CHAR_LEFT_WITHOUT_NOTIFYING 20000
+
 int main()
 {
     printf("This is a basic steganography program.\n");
@@ -70,73 +85,81 @@ int printMenu()
 
 void hideMessage()
 {
-    char input_fpath[100];
-    char output_fpath[100];
+    char fpath[100];
     FILE *ifp, *ofp;
 
     printf("Please provide a path for the input file (.bmp):\n");
-    scanf("%s", input_fpath);
-    ifp = fopen(input_fpath, "r");
+    scanf("%s", fpath);
+    ifp = fopen(fpath, "rb");
 
     while (ifp == NULL)
     {
         printf("Could not open file. Please provide a path for the input file (.bmp):\n");
-        scanf("%s", input_fpath);
-        ifp = fopen(input_fpath, "r");
+        scanf("%s", fpath);
+        ifp = fopen(fpath, "rb");
     }
 
     printf("Please provide a path to the output file (include .bmp):\n");
-    scanf("%s", output_fpath);
+    scanf("%s", fpath);
 
-    ofp = fopen(output_fpath, "w");
+    ofp = fopen(fpath, "wb");
 
     while (ofp == NULL)
     {
         printf("Could not open or create the file. Please provide a path for the output file (include .bmp):\n");
-        scanf("%s", output_fpath);
-        ifp = fopen(output_fpath, "w");
+        scanf("%s", fpath);
+        ifp = fopen(fpath, "wb");
     }
 
     // get size of input file
     fseek(ifp, 0L, SEEK_END);
-    unsigned long long input_file_size = ftell(ifp);
+    unsigned long long inputFileSize = ftell(ifp);
 
     // bring ifp back to the beginning of the file
     rewind(ifp);
 
     //print the file size
     printf("Input file size: ");
-    printPrettyFileSize(input_file_size);
+    printPrettyNumber(inputFileSize, BYTES);
     printf("\n");
 
     // Get terminating character(s)
-    printf("What are the 4 terminating characters for your message? (These will not be included)\n");
+    printf("What are the 4 terminating characters for your message (ex. XXXX)? (These will not be included)\n");
     char terminatingChars[5];
     scanf("%s", terminatingChars);
 
     // Ask for message:
     fflush(stdin);
+    unsigned long long maxMessageSize = (inputFileSize - (HEADER_SIZE + INFO_HEADER_SIZE + MESSAGE_SIZE_SECTION_SIZE)) / 8ull;
     printf("What message would you like to hide? You're limited to ");
-    printPrettyFileSize(input_file_size / 8ull);
-    printf(" (%llu characters)", input_file_size / 8ull);
+    printPrettyNumber(maxMessageSize, BYTES);
+    printf(" (%llu characters)", maxMessageSize);
     printf(":\n");
 
     // Read secret message
-    unsigned long long messageAllocSize = 5000ull;
-    unsigned long long messageSize = 0;
+    unsigned long long messageAllocSize = (maxMessageSize < 5000ull) ? (maxMessageSize + 1) : 5000ull;
     char *message = malloc(messageAllocSize);
-    char *inputBuffer;
-    int keepReading = 1;
-    unsigned long long charIdx;
+    unsigned long long messageSize = 0ull;
 
+    // message input buffer variables
+    char *inputBuffer = malloc(MAX_INPUT_LINE_LENGTH);
+    int inputIdx;
+
+    int keepReading = 1;
     while (keepReading)
     {
-        messageAllocSize = 5000ull;
-        unsigned long long bufferSize = 100000ul;
-        inputBuffer = malloc(bufferSize);
-        charIdx = 0ull;
+        // print number of chars remaining
+        if (maxMessageSize - messageSize < MIN_CHAR_LEFT_WITHOUT_NOTIFYING)
+        {
+            printf("\n(chars left: ");
+            printPrettyNumber(maxMessageSize - messageSize, SUFFIX);
+            printf(")\n");
+        }
 
-        for (unsigned long long idx = charIdx; idx < bufferSize; idx++)
+        inputIdx = 0;
+
+        // initialize input buffer with terminating characters
+        for (unsigned long long idx = inputIdx; idx < MAX_INPUT_LINE_LENGTH; idx++)
         {
             inputBuffer[idx] = '\0';
         }
@@ -144,41 +167,37 @@ void hideMessage()
         // read in characters until we hit a new line character
         scanf(" %[^\n]s", inputBuffer);
 
-        while (inputBuffer[charIdx] != '\0')
+        while (inputBuffer[inputIdx] != '\0')
         {
-            // check whether this starts the terminating characters
+            // check for the terminating characters
             keepReading = 0;
             for (int i = 0; i < 4; i++)
             {
-                if (inputBuffer[charIdx + i] != terminatingChars[i])
+                if (inputBuffer[inputIdx + i] != terminatingChars[i])
                 {
                     keepReading = 1;
                     break;
                 }
             }
-
             if (!keepReading)
-            {
                 break;
-            }
 
             // make sure we have enough memory
-            if (messageSize >= messageAllocSize / 2ull)
+            if (inputIdx >= messageAllocSize / 2ull)
             {
                 messageAllocSize *= 2ull;
             }
 
             // make sure the user didn't use too many characters
-            if (messageSize >= input_file_size / 8)
+            if (messageSize >= maxMessageSize)
             {
-                printf("Your message has been truncated because it used too many characters");
+                printf("Your message has been truncated because it used too many characters.");
                 keepReading = 0;
                 break;
             }
 
             // store message
-            message[messageSize++] = inputBuffer[charIdx];
-            charIdx++;
+            message[messageSize++] = inputBuffer[inputIdx++];
         }
 
         // add new line char
@@ -186,73 +205,75 @@ void hideMessage()
         {
             message[messageSize++] = '\n';
         }
-
-        free(inputBuffer);
     }
 
+    free(inputBuffer);
+
+    // print message size
     printf("Message Size: ");
-    printPrettyFileSize(messageSize);
-    printf(" (%lu characters)\n", messageSize);
+    printPrettyNumber(messageSize, BYTES);
+    printf(" (");
+    printPrettyNumber(messageSize, SUFFIX);
+    printf(" characters)\n");
 
-    unsigned long messageByteIdx = 0;
-    int messageBitIdx = 0;
-    char nextByteInput = fgetc(ifp);
-    char altByte;
-    char bitMask;
-
-    int headerBytesPrinted = 0;
-    int messageSizeBytesPrinted = 0, messageSizeBitsPrinted = 0;
-
-    fseek(ifp, 1L, SEEK_SET);
+    // set pointers to beginning of each file
+    fseek(ifp, 0L, SEEK_SET);
     fseek(ofp, 0L, SEEK_SET);
-    int error = 0;
-    unsigned long long inputBytesRead = 0ull;
-    while (inputBytesRead++ < input_file_size)
+
+    unsigned char buffer[STD_BUFFER_SIZE];
+
+    // Copy the header of the file
+    fread(buffer, HEADER_SIZE + INFO_HEADER_SIZE, 1, ifp);
+    fwrite(buffer, HEADER_SIZE + INFO_HEADER_SIZE, 1, ofp);
+
+    // write the message length
+    fread(buffer, MESSAGE_SIZE_SECTION_SIZE, 1, ifp);
+    for (int byteIdx = 0; byteIdx < 8; byteIdx++)
     {
-        // Copy byte from original file, do not modify, iff:
-        // |- Copying remaining Bytes -|  OR  |--- copying header ---|
-        if (messageByteIdx >= messageSize || headerBytesPrinted++ < 54)
+        for (int bitIdx = 0; bitIdx < 8; bitIdx++)
         {
-            if (fputc(nextByteInput, ofp) == EOF && error == 0)
-            {
-                error = 1;
-                printf("\n--- ERROR: ---\n\tCould not copy byte from original to new file. (either in header or remainder of file\n\n");
-            }
+            int bitPosition = (8 * byteIdx + bitIdx);
+            buffer[bitPosition] = (buffer[bitPosition] & ~1) | ((char)((messageSize & (0x0001 << (63 - bitPosition))) >> (63 - bitPosition)));
         }
-        // 2. print the size of the message (unsigned long)
-        else if (messageSizeBytesPrinted < 8)
-        {
-            int bitPosition = (8 * messageSizeBytesPrinted + messageSizeBitsPrinted);
-            altByte = (nextByteInput & ~1) | ((char)((messageSize & (0x0001 << (63 - bitPosition))) >> (63 - bitPosition)));
-
-            fputc(altByte, ofp);
-
-            // increment message indices
-            messageSizeBitsPrinted++;
-            if (messageSizeBitsPrinted >= 8)
-            {
-                messageSizeBitsPrinted = 0;
-                messageSizeBytesPrinted++;
-            }
-        }
-        else
-        {
-            altByte = (nextByteInput & ~1) | ((char)((message[messageByteIdx] & (0x0001 << (7 - messageBitIdx))) >> (7 - messageBitIdx)));
-            fputc(altByte, ofp);
-
-            // increment message indices
-            messageBitIdx++;
-            if (messageBitIdx >= 8)
-            {
-                messageBitIdx = 0;
-                messageByteIdx++;
-            }
-        }
-
-        nextByteInput = fgetc(ifp);
     }
+    fwrite(buffer, MESSAGE_SIZE_SECTION_SIZE, 1, ofp);
 
+    // Hide message
+    unsigned long long messageBytesWritten = 0ull;
+    int MESSAGE_BYTES_WRITABLE_PER_ITER = (sizeof(buffer) / 8);
+    int messageBytesToWrite, inputBytesToWrite;
+    while (messageBytesWritten < messageSize)
+    {
+        // calculate the number of *message* bytes we want to write
+        // NOTE: One message byte is written across the LSBs of *8 input bytes*
+        messageBytesToWrite = ((messageSize - messageBytesWritten) < MESSAGE_BYTES_WRITABLE_PER_ITER) ? (messageSize - messageBytesWritten) : MESSAGE_BYTES_WRITABLE_PER_ITER;
+
+        // read input file bytes - 8 for each message byte
+        inputBytesToWrite = messageBytesToWrite * 8;
+        fread(buffer, inputBytesToWrite, 1, ifp);
+
+        // for each byte of the input, hide a bit of the message in the LSB
+        for (int byteIdx = 0; byteIdx < messageBytesToWrite; byteIdx++)
+        {
+            for (int bitIdx = 0; bitIdx < 8; bitIdx++)
+            {
+                int inputByteNumber = (8 * byteIdx + bitIdx);
+                buffer[inputByteNumber] = (buffer[inputByteNumber] & ~1) | ((char)((message[messageBytesWritten + byteIdx] & (0x0001 << (7 - bitIdx))) >> (7 - bitIdx)));
+            }
+        }
+        // write output file bytes -> same # as input bytes
+        fwrite(buffer, inputBytesToWrite, 1, ofp);
+
+        messageBytesWritten += messageBytesToWrite;
+    }
     free(message);
+
+    // write the remainder of the file
+    unsigned long long totalBytesWritten = ftell(ifp);
+
+    if (inputFileSize > totalBytesWritten)
+        copyBytes(ifp, ofp, inputFileSize - totalBytesWritten);
+
     fclose(ifp);
     fclose(ofp);
     printf("\nFinished!\n\n");
@@ -291,47 +312,71 @@ void printMessage()
 
     // get size of input file
     fseek(ifp, 0L, SEEK_END);
-    unsigned long input_file_size = ftell(ifp);
+    unsigned long inputFileSize = ftell(ifp);
 
     // bring ifp back to the beginning of the file
     rewind(ifp);
 
     //print the file size
     printf("Input file size: ");
-    printPrettyFileSize(input_file_size);
+    printPrettyNumber(inputFileSize, BYTES);
     printf("\n");
 
     // go to 53 bytes past the beginning of the file (skip the header)
     fseek(ifp, 54L, SEEK_SET);
 
-    unsigned long messageSize = 0ul;
+    // read the message size
+    unsigned long long messageSize = 0ull;
+    unsigned char buffer[STD_BUFFER_SIZE];
+    fread(buffer, MESSAGE_SIZE_SECTION_SIZE, 1, ifp);
 
     int nextByteInput, bitVal;
     // read the message size
-    for (int bitIdx = 0; bitIdx < 64; bitIdx++)
+    for (int bitIdx = 0; bitIdx < MESSAGE_SIZE_SECTION_SIZE; bitIdx++)
     {
-        nextByteInput = fgetc(ifp);
+        nextByteInput = buffer[bitIdx];
         messageSize = messageSize | ((nextByteInput & 1) << (63 - bitIdx));
     }
     printf("Message size: ");
-    printPrettyFileSize(messageSize);
+    printPrettyNumber(messageSize, BYTES);
     printf(" (%lu characters)\n", messageSize);
 
     // read message
-    printf("\nHidden message: \n\n");
-    for (unsigned long bytesRead = 0ul; bytesRead < messageSize; bytesRead++)
+    unsigned long long messageBytesRead = 0ull;
+    int MESSAGE_BYTES_READABLE_PER_ITER = sizeof(buffer) / 8;
+    int messageBytesToRead, inputBytesToRead, bufferIdx;
+    while (messageBytesRead < messageSize)
     {
-        char charASCII = 0;
-        for (int bitsRead = 0; bitsRead < 8; bitsRead++)
-        {
-            nextByteInput = fgetc(ifp);
-            charASCII = charASCII | ((nextByteInput & 1) << (7 - bitsRead));
-        }
+        // calculate the number of *message* bytes we want to read
+        // NOTE: One message byte is written across the LSBs of *8 input bytes*
+        messageBytesToRead = ((messageSize - messageBytesRead) < MESSAGE_BYTES_READABLE_PER_ITER) ? (messageSize - messageBytesRead) : MESSAGE_BYTES_READABLE_PER_ITER;
 
-        fputc(charASCII, ofp);
-        printf("%c", charASCII);
+        // read input file bytes - 8 for each message byte
+        inputBytesToRead = messageBytesToRead * 8;
+        fread(buffer, inputBytesToRead, 1, ifp);
+
+        // for each byte of the input, read a bit of the message in the LSB
+        for (int byteIdx = 0; byteIdx < messageBytesToRead; byteIdx++)
+        {
+            char messageByte = 0x00;
+            for (int bitIdx = 0; bitIdx < 8; bitIdx++)
+            {
+                bufferIdx = 8 * byteIdx + bitIdx;
+                messageByte = messageByte | ((buffer[bufferIdx] & 1) << (7 - bitIdx));
+            }
+            // overwrite beginning of buffer as we "decode" the message
+            buffer[byteIdx] = messageByte;
+
+            // print decoded char to screen
+            printf("%c", messageByte);
+        }
+        // write output file bytes -> same # as *message* bytes
+        fwrite(buffer, messageBytesToRead, 1, ofp);
+
+        messageBytesRead += messageBytesToRead;
     }
     printf("\n\n");
+
     fclose(ifp);
     fclose(ofp);
     return;
@@ -368,40 +413,39 @@ void duplicateFile()
 
     // get size of input file
     fseek(ifp, 0L, SEEK_END);
-    unsigned long long input_file_size = ftell(ifp);
+    unsigned long long inputFileSize = ftell(ifp);
 
     // bring ifp back to the beginning of the file
     rewind(ifp);
 
     //print the file size
     printf("Input file size: ");
-    printPrettyFileSize(input_file_size);
+    printPrettyNumber(inputFileSize, BYTES);
     printf("\n\n");
 
-    // Copy contents to new file
-    unsigned long long bytesRead = 0ull;
-    unsigned char byteBuffer[COPY_BUFFER_SIZE];
-    int f = 0;
-    while (bytesRead < input_file_size)
-    {
-        if (input_file_size - bytesRead <= COPY_BUFFER_SIZE)
-        {
-            fread(byteBuffer, input_file_size - bytesRead, 1, ifp);
-            fwrite(byteBuffer, input_file_size - bytesRead, 1, ofp);
-            break;
-        }
-        fread(byteBuffer, sizeof(byteBuffer), 1, ifp);
-        fwrite(byteBuffer, sizeof(byteBuffer), 1, ofp);
-
-        bytesRead += COPY_BUFFER_SIZE;
-    }
+    copyBytes(ifp, ofp, inputFileSize);
 
     fclose(ifp);
     fclose(ofp);
     return;
 }
 
-void printPrettyFileSize(unsigned long long size)
+void copyBytes(FILE *ifp, FILE *ofp, unsigned long long numBytes)
+{
+    // Copy contents to new file
+    unsigned long long bytesCopied = 0ull;
+    unsigned char byteBuffer[STD_BUFFER_SIZE];
+
+    while (bytesCopied < numBytes)
+    {
+        int bytesToCopy = (numBytes - bytesCopied <= sizeof(byteBuffer)) ? (numBytes - bytesCopied) : sizeof(byteBuffer);
+        fread(byteBuffer, bytesToCopy, 1, ifp);
+        fwrite(byteBuffer, bytesToCopy, 1, ofp);
+        bytesCopied += bytesToCopy;
+    }
+}
+
+void printPrettyNumber(unsigned long long size, int format)
 {
     char pretty[10];
 
@@ -410,31 +454,69 @@ void printPrettyFileSize(unsigned long long size)
     {
         double numGB = (double)(size / 10000000ull); // # bytes / 10,000,000 Bytes
         numGB /= 100.0;
-        printf("%.2fGB", numGB);
+        printf("%.2f", numGB);
+
+        switch (format)
+        {
+        case BYTES:
+            printf("GB");
+            break;
+        case SUFFIX:
+            printf("B");
+            break;
+        default:
+            break;
+        }
     }
     else if (size / 1000000ull > 0)
     {
         double numMB = (double)(size / 10000ull); // # bytes / 10,000
         numMB /= 100.0;
-        printf("%.2fMB", numMB);
+        printf("%.2f", numMB);
+
+        switch (format)
+        {
+        case BYTES:
+            printf("MB");
+            break;
+        case SUFFIX:
+            printf("M");
+            break;
+        default:
+            break;
+        }
     }
     else if (size / 1000ull > 0)
     {
         double numKB = (double)(size / 10000.0);
-        printf("%.2fKB", numKB);
+        printf("%.2f", numKB);
+
+        switch (format)
+        {
+        case BYTES:
+            printf("KB");
+            break;
+        case SUFFIX:
+            printf("K");
+            break;
+        default:
+            break;
+        }
     }
     else
     {
-        printf("%d bytes", (unsigned int)size);
+        printf("%d", (unsigned int)size);
+
+        switch (format)
+        {
+        case BYTES:
+            printf(" bytes");
+            break;
+        case SUFFIX:
+            break;
+        default:
+            break;
+        }
     }
     return;
-}
-
-void printCharBinary(char in)
-{
-    int i;
-    for (i = 0; i < 8; i++)
-    {
-        printf("%d", !!((in << i) & 0x80));
-    }
 }
